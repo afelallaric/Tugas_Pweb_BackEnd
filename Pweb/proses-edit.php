88477ab34b34b2ef73b1f32f5a643a75397f25cb
Afel <162775751+afelallaric@users.noreply.github.com>
2024-12-11 15:22:35 +0700

    Add files via upload

---
+++ Pweb/proses-edit.php
@@ -0,0 +1,26 @@
+<?php
+
+require 'config.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = $_POST['id'];
+    $nama = $_POST['nama'];
+    $alamat = $_POST['alamat'];
+    $jenis_kelamin = $_POST['jenis_kelamin'];
+    $agama = $_POST['agama'];
+    $sekolah_asal = $_POST['sekolah_asal'];
+
+    $sql = "UPDATE calon_siswa SET 
+                nama = '$nama', 
+                alamat = '$alamat', 
+                jenis_kelamin = '$jenis_kelamin', 
+                agama = '$agama', 
+                sekolah_asal = '$sekolah_asal' 
+            WHERE id = $id";
+
+    if (mysqli_query($conn, $sql)) {
+        header('Location: list-siswa.php');
+    } else {
+        echo "Gagal mengupdate data: " . mysqli_error($conn);
+    }
+}
